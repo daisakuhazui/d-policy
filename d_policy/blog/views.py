3067bf05db3043e902c3939b37670bebb544c6ec
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-07-03 16:47:15 +0900

    Post-view(index, detail) done

--- d_policy/blog/views.py
+++ d_policy/blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+class PostIndex(generic.ListView):
+    model = Post
+    template_name = 'blog/index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
